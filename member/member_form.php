e123dc9322a70a3adff6dee797ced5b59c6d30f2
yummycloud <ygni1008@gmail.com>
2022-10-25 10:38:04 +0900

    githup_commit_10251037

--- member/member_form.php
+++ member/member_form.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>KGITBANK</title>
+    <title>에너자이조</title>
     <link rel="stylesheet" type="text/css" href="../css/common.css">
     <link rel="stylesheet" type="text/css" href="../css/member.css">
     <link href="../css/bootstrap.min.css" rel="stylesheet">
